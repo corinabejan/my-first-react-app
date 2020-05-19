b89cba2f0e8940c841c2b24f6e478034b8a650da
corina <corinabe93@gmail.com>
2020-05-19 20:05:53 +0200

    styling added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Pokemon from "./components/Pokemon/Pokemon";
+import Title from "./components/Title";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
+      <main className="container">
+        <Title content="Some Simple Title" />
+        <div className="row">
+          {all_pokemon.map((pokemon) => (
+            <div className="col-md-6 col-lg-4">
+              <Pokemon
+                name={pokemon.name}
+                weight={pokemon.weight}
+                awesome={pokemon.awesome}
+                terrifying={pokemon.terrifying}
+                abilities={pokemon.abilities}
+              />
+            </div>
+          ))}
+        </div>
+      </main>
       <header className="App-header">
         <p>Hello Codaisseur</p>
         <img src={logo} className="App-logo" alt="logo" />
